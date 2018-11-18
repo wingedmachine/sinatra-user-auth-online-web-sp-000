d69f6efacf4f217c517a36ca0bf7d4f10e9d997f
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-18 18:34:48 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,9 +30,10 @@ class ApplicationController < Sinatra::Base
 
   post '/sessions' do
     @user = User.find_by(email: params["email"], password: params["password"])
-    session[:id] = @user.id
-
-    redirect '/users/home'
+    # session[:id] = @user.id
+    # 
+    # redirect '/users/home'
+    redirect '/sessions/login'
   end
 
   get '/sessions/logout' do
