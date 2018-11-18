b3d26223caa319c098359bb392b32a93b19d7083
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-18 18:23:50 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
   post '/registrations' do
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
-binding.pry
+
     session[:id] = @user.id
 
     redirect '/users/home'
